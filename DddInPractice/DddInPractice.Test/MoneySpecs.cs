daa7c9e380005fcded9c75313cd5bede77edfd25
Petru Betco <petrubetco@gmail.com>
2019-06-05 18:02:42 +0300

    initial commit

---
+++ DddInPractice/DddInPractice.Test/MoneySpecs.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace DddInPractice.Test
+{
+    public class MoneySpecs
+    {
+        [Fact]
+        public void Sum_of_two_moneys_produce_correct_result()
+        {
+            //Arrange 
+            var money1 = new Money(1,2,3,4,5,6);
+            var money2 = new Money(1,2,3,4,5,6);
+
+            //Act 
+            var sum = money1 + money2;
+
+            //Assert
+
+            //Assert.Equal(2, sum.OneCentCount); - we will use FluentAssertions instead
+            sum.OneCentCount.Should().Be(2);
+            sum.TenCentCount.Should().Be(4);
+            sum.QuartersCount.Should().Be(6);
+            sum.OneDollarCount.Should().Be(8);
+            sum.FiveDollarsCount.Should().Be(10);
+            sum.TwentyDollarsCount.Should().Be(12);
+        }
+
+        [Fact]
+        public void Two_money_instances_equal_if_they_contain_the_same_amount()
+        {
+
+        }
+    }
+}
---
+++ DddInPractice/DddInPractice/Money.cs
@@ -0,0 +1,67 @@
+﻿
+namespace DddInPractice
+{
+    public sealed class Money : ValueObject<Money>
+    {
+        public int OneCentCount { get; set; }
+        public int TenCentCount { get; set; }
+        public int QuartersCount { get; set; }
+        public int OneDollarCount { get; set; }
+        public int FiveDollarsCount { get; set; }
+        public int TwentyDollarsCount { get; set; }
+
+        public Money(
+            int oneCentCount,
+            int tenCentCount,
+            int quartersCount,
+            int oneDollarCount,
+            int fiveDollarsCount,
+            int twentyDollarsCount)
+        {
+            OneCentCount = oneCentCount;
+            TenCentCount = tenCentCount;
+            QuartersCount = quartersCount;
+            OneDollarCount = oneDollarCount;
+            FiveDollarsCount = fiveDollarsCount;
+            TwentyDollarsCount = twentyDollarsCount;
+        }
+
+        public static Money operator +(Money money1, Money money2)
+        {
+            var sum = new Money(
+                money1.OneCentCount + money2.OneCentCount,
+                money1.TenCentCount + money2.TenCentCount,
+                money1.QuartersCount + money2.QuartersCount,
+                money1.OneDollarCount + money2.OneDollarCount,
+                money1.FiveDollarsCount + money2.FiveDollarsCount,
+                money1.TwentyDollarsCount + money2.TwentyDollarsCount);
+
+            return sum;
+        }
+
+        protected override bool EqualsCore(Money other)
+        {
+            return OneCentCount == other.OneCentCount
+                   && TenCentCount == other.TenCentCount
+                   && QuartersCount == other.QuartersCount
+                   && OneDollarCount == other.OneDollarCount
+                   && FiveDollarsCount == other.FiveDollarsCount
+                   && TwentyDollarsCount == other.TwentyDollarsCount;
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            unchecked
+            {
+                var hashCode = OneCentCount;
+                hashCode = (hashCode * 397) ^ TenCentCount;
+                hashCode = (hashCode * 397) ^ QuartersCount;
+                hashCode = (hashCode * 397) ^ OneDollarCount;
+                hashCode = (hashCode * 397) ^ FiveDollarsCount;
+                hashCode = (hashCode * 397) ^ TwentyDollarsCount;
+
+                return hashCode;
+            }
+        }
+    }
+}
